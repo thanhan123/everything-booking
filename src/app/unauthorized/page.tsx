51e12f7d3957db9f7cdbd10e1e8ccd3f13dea392
An Dinh <thanhan.uit@gmail.com>
2025-11-04 20:52:58 +0700

    change source to deploy

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -2,7 +2,7 @@ export default function UnauthorizedPage() {
     return (
         <div className="flex h-screen flex-col items-center justify-center">
             <h1 className="text-3xl font-bold mb-4">🚫 Access Denied</h1>
-            <p>You don’t have permission to view this page.</p>
+            <p>You don’t have permission to view this page. Test</p>
         </div>
     )
 }
